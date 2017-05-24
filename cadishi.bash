294d658e4f2df7b5e522f1dd3143729308e31dbd
Klaus Reuter <khr@rzg.mpg.de>
2017-05-24 12:36:31 +0200

    improved bash command completion file

--- cadishi.bash
+++ cadishi.bash
@@ -8,14 +8,16 @@
 
 _cadishi()
 {
-    local cur prev opts histo_opts histo_example_opts
+    local cur prev opts histo_opts example_opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    opts="histo histo-example -h --help"
-    histo_opts="-h --help --input"
-    histo_example_opts="-h --help --output"
+    opts="histo example merge unpack --help --version"
+    histo_opts="--help --input"
+    example_opts="--help --output"
+    merge_opts="--help --force --output"
+    unpack_opts="--help --force --output"
 
     if [[ ${prev} == --input ]] ; then
         COMPREPLY=( $(compgen -f -X '!*.yaml' -- ${cur}) )
@@ -29,8 +31,16 @@ _cadishi()
         COMPREPLY=( $(compgen -W "${histo_opts}" -- ${cur}) )
         return 0
     fi
-    if [[ ${prev} == histo-example ]] ; then
-        COMPREPLY=( $(compgen -W "${histo_example_opts}" -- ${cur}) )
+    if [[ ${prev} == example ]] ; then
+        COMPREPLY=( $(compgen -W "${example_opts}" -- ${cur}) )
+        return 0
+    fi
+    if [[ ${prev} == merge ]] ; then
+        COMPREPLY=( $(compgen -W "${merge_opts}" -- ${cur}) )
+        return 0
+    fi
+    if [[ ${prev} == unpack ]] ; then
+        COMPREPLY=( $(compgen -W "${unpack_opts}" -- ${cur}) )
         return 0
     fi
     if [[ ${cur} == * ]] ; then
