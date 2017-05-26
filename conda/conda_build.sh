f00c7283859bc2534e8805f15ac6c86298fab198
Klaus Reuter <khr@rzg.mpg.de>
2017-05-26 08:16:22 +0200

    commented conda build, added simple import test

--- conda/conda_build.sh
+++ conda/conda_build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -e
+
+# trick to determine the absolute directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# where to put the output (the packaged file)
 CONDA_BLD_OUTDIR=$DIR/output/
 mkdir -p $CONDA_BLD_OUTDIR
 
+# for some reason there must be no overlap between the package source path
+# and the build path, so we put it into /tmp.
+# Note that the directory is removed below using "rm -rf"!
 export CONDA_BLD_PATH=/tmp/$USER/conda-bld
 mkdir -p $CONDA_BLD_PATH
 
 conda build --no-anaconda-upload --output-folder $CONDA_BLD_OUTDIR ./recipe/
+
+rm -rf "$CONDA_BLD_PATH"
--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,2 +1,3 @@
 export CAD_CUDA=0
+export CAD_OPENMP=1
 $PYTHON setup.py config build install
