e37a48b3575b6555f2a8b7c8e3af229d9025cec5
Klaus Reuter <khr@rzg.mpg.de>
2017-08-22 16:21:47 +0200

    added license header to .bash file

--- cadishi.bash
+++ cadishi.bash
@@ -1,10 +1,17 @@
+# -*- tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# Cadishi --- CAlculation of DIStance HIstograms
+#
+# Copyright (c) Klaus Reuter, Juergen Koefinger
+# See the file AUTHORS.rst for the full list of contributors.
+#
+# Released under the MIT License, see the file LICENSE.txt.
+
 # Bash command completion for cadishi.
 #
 # `source` this file into a bash session (e.g. automatically using .bashrc)
 # and use the TAB key to get command suggestions.
-#
-# This file is part of the Cadishi package.  See README.rst,
-# LICENSE.txt, and the documentation for details.
 
 _cadishi()
 {
