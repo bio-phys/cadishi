b44351fcb77737c6a6da5249a0c24ee8e34f72d2
Klaus Reuter <khr@mpcdf.mpg.de>
2020-09-16 21:06:09 +0200

    another version pointrelease due to pypi

--- scripts/pypi_upload_helper.sh
+++ scripts/pypi_upload_helper.sh
@@ -4,8 +4,7 @@
 
 python setup.py clean
 
-/opt/apps/anaconda/3/current/bin/python3 setup.py sdist
-#/opt/apps/anaconda/3/4.2.0/bin/python3 setup.py bdist_wheel --universal
+python3 setup.py sdist
 
 echo "NOW RUN:  twine upload dist/*"
 
